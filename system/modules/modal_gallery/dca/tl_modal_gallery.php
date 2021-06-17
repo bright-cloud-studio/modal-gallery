6b57167a3c02d24c5eaf47b40888433a2348c714
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-06-17 14:50:05 -0400

    Update tl_modal_gallery.php

--- system/modules/modal_gallery/dca/tl_modal_gallery.php
+++ system/modules/modal_gallery/dca/tl_modal_gallery.php
@@ -47,64 +47,64 @@ $GLOBALS['TL_DCA']['tl_modal_gallery'] = array
 			'fields'		=> array('title'),
 			'panelLayout'		=> 'filter;search,limit'
         	),
-        'label' => array
-        (
-            'fields'                  => array('title'),
-            'format'                  => '%s'
-        ),
-        'global_operations' => array
-        (
-            'all' => array
-            (
-                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
-                'href'                => 'act=select',
-                'class'               => 'header_edit_all',
-                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
-            )
-        ),
-        'operations' => array
-        (
-		'edit' => array
+		'label' => array
 		(
-			'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['edit'],
-			'href'                => 'table=tl_modal_gallery_slide',
-			'icon'                => 'edit.svg'
+		    'fields'                  => array('title'),
+		    'format'                  => '%s'
 		),
-		'editheader' => array
+		'global_operations' => array
 		(
-			'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['editheader'],
-			'href'                => 'act=edit',
-			'icon'                => 'header.svg',
-			'button_callback'	=> array('tl_modaL_gallery', 'editHeader')
+		    'all' => array
+		    (
+			'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+			'href'                => 'act=select',
+			'class'               => 'header_edit_all',
+			'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+		    )
 		),
-            'copy' => array
-            (
-                'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['copy'],
-                'href'                => 'act=copy',
-                'icon'                => 'copy.gif'
-            ),
-            'delete' => array
-            (
-                'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['delete'],
-                'href'                => 'act=delete',
-                'icon'                => 'delete.gif',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
-            ),
-            'toggle' => array
-	    (
-		'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['toggle'],
-		'icon'                => 'visible.gif',
-		'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
-		'button_callback'     => array('Bcs\Backend\ModalGalleryBackend', 'toggleIcon')
-	    ),
-            'show' => array
-            (
-                'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['show'],
-                'href'                => 'act=show',
-                'icon'                => 'show.gif'
-            )
-        )
-    ),
+		'operations' => array
+		(
+			'edit' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['edit'],
+				'href'                => 'table=tl_modal_gallery_slide',
+				'icon'                => 'edit.svg'
+			),
+			'editheader' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['editheader'],
+				'href'                => 'act=edit',
+				'icon'                => 'header.svg',
+				'button_callback'	=> array('tl_modaL_gallery', 'editHeader')
+			),
+		    'copy' => array
+		    (
+			'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['copy'],
+			'href'                => 'act=copy',
+			'icon'                => 'copy.gif'
+		    ),
+		    'delete' => array
+		    (
+			'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['delete'],
+			'href'                => 'act=delete',
+			'icon'                => 'delete.gif',
+			'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+		    ),
+		    'toggle' => array
+		    (
+			'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['toggle'],
+			'icon'                => 'visible.gif',
+			'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+			'button_callback'     => array('Bcs\Backend\ModalGalleryBackend', 'toggleIcon')
+		    ),
+		    'show' => array
+		    (
+			'label'               => &$GLOBALS['TL_LANG']['tl_modal_gallery']['show'],
+			'href'                => 'act=show',
+			'icon'                => 'show.gif'
+		    )
+		)
+    	),
  
     // Palettes
     'palettes' => array
