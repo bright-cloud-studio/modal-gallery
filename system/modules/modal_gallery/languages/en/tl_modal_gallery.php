33573701e2d09e2822708599ed2281c80bec321c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-06-23 11:55:41 -0400

    Update tl_modal_gallery.php

--- system/modules/modal_gallery/languages/en/tl_modal_gallery.php
+++ system/modules/modal_gallery/languages/en/tl_modal_gallery.php
@@ -12,33 +12,20 @@
 
 
 /* Legends */
-$GLOBALS['TL_LANG']['tl_modal_gallery']['slide_legend']        	= 'Image Details';
-$GLOBALS['TL_LANG']['tl_modal_gallery']['hotspot_legend']      	= 'Hotspot Details';
+$GLOBALS['TL_LANG']['tl_modal_gallery']['slide_legend']        	= 'Gallery Details';
 $GLOBALS['TL_LANG']['tl_modal_gallery']['publish_legend']      	= 'Publish';
 
 /* Fields */
-$GLOBALS['TL_LANG']['tl_modal_gallery']['alias'] 			            = array('Alias', 'Auto-generated alias.');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['product_name'] 			     = array('Product Name', 'The name of the product');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['product_inventory'] 	 	= array('Product Inventory', 'The inventory total');
 
-$GLOBALS['TL_LANG']['tl_modal_gallery']['slide_image'] 	      		= array('Select the Slide\'s Image', 'Select the image we will use for this particular slide');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['slide_image_size'] 	   = array('Select the Slide\'s Image\'s Size', 'Set the slide images size settings');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['slide_image_meta'] 	   = array('Select the Slide\'s Image\'s Meta', 'Set the meta details');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['slide_name'] 	       		= array('Slide Name', 'Give this slide a name');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['hotspot_icon'] 	      	= array('Hotspot Icon', 'Insert code for the hotspot links\'s icon');
+$GLOBALS['TL_LANG']['tl_modal_gallery']['modal_gallery_title']                = array('Gallery Name', 'Give this gallery a name');
+$GLOBALS['TL_LANG']['tl_modal_gallery']['modal_gallery_hotspot_icon'] 	      	= array('Hotspot Icon', 'Insert an HTML i tag which will be the icon for hotspots');
 
-$GLOBALS['TL_LANG']['tl_modal_gallery']['hotspot_links']     	 	= array('Hotspot Links', 'The hotspot links that will be on the slide');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['hotspot_x']         	 	= array('Hotspot X', 'X coordinate for the hotspot link on the image');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['hotspot_y'] 	        	 = array('Hotspot Y', 'Y coordinate for the hotspot link on the image');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['hotspot_title']      		= array('Hotspot Title', 'The text that will be the title for the modal box');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['hotspot_text']      	 	= array('Hotspot Text', 'The text that will show in the modal box');
 
 $GLOBALS['TL_LANG']['tl_modal_gallery']['published'] 		         = array('Published', 'Save this product\'s record.');
  
  /* Buttons */
-$GLOBALS['TL_LANG']['tl_modal_gallery']['new']   		 			         = array('New Slide', 'Add a new slide');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['show']  		 		         	= array('Slide Details', 'Show the details of slide ID %s');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['edit']  		          			= array('Edit Slide', 'Edit slide ID %s');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['copy']  		 	         		= array('Copy Slide', 'Copy slide ID %s');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['delete'] 		         			= array('Delete Slide', 'Delete slide ID %s');
-$GLOBALS['TL_LANG']['tl_modal_gallery']['toggle'] 			         		= array('Toggle Slide Published', 'Toggle slide ID %s');
+$GLOBALS['TL_LANG']['tl_modal_gallery']['new']   		 			         = array('New Gallery', 'Add a new gallery');
+$GLOBALS['TL_LANG']['tl_modal_gallery']['edit']  		          			= array('Edit Gallery', 'Edit gallery ID: %s');
+$GLOBALS['TL_LANG']['tl_modal_gallery']['copy']  		 	         		= array('Copy Gallery', 'Copy gallery ID: %s');
+$GLOBALS['TL_LANG']['tl_modal_gallery']['delete'] 		         			= array('Delete Gallery', 'Delete gallery ID: %s');
+$GLOBALS['TL_LANG']['tl_modal_gallery']['toggle'] 			         		= array('Toggle Gallery Published', 'Toggle gallery ID: %s');
