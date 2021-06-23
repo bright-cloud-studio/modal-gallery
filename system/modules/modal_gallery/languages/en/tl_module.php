b24845275c6ee65af7ed4373fe31d588d9647491
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-06-23 11:31:10 -0400

    Create tl_module.php

---
+++ system/modules/modal_gallery/languages/en/tl_module.php
@@ -0,0 +1,9 @@
+
+<?php
+ 
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_module']['selectedGallery'] 		= array('Select Gallery', 'Select which gallery to display.');
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_module']['select_gallery_legend'] 	= array('Pick A Gallery', 'Pick which gallery this module will show.');
