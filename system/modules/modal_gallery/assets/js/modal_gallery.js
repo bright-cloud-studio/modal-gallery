7a2d80a3e91dd61ec0e216b20932f66854515704
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-06-24 11:18:16 -0400

    Update modal_gallery.js

--- system/modules/modal_gallery/assets/js/modal_gallery.js
+++ system/modules/modal_gallery/assets/js/modal_gallery.js
@@ -32,9 +32,11 @@ function showSlides(n) {
 		}
 		for (i = 0; i < dots.length; i++) {
 			dots[i].className = dots[i].className.replace(" active", "");
+			dots[i].parentElement.className = dots[i].parentElement.className.replace(" active", "");
 		}
 		slides[slideIndex-1].style.display = "block";
 		dots[slideIndex-1].className += " active";
+		dots[slideIndex-1].parentElement.className += " active";
 		captionText.innerHTML = dots[slideIndex-1].alt;
 	}
 }
