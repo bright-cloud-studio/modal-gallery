7a3cbe2a513df3b923727d1cc743463ae4f08858
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-13 10:30:33 -0500

    Update modules.php

--- system/modules/modal_gallery/languages/en/modules.php
+++ system/modules/modal_gallery/languages/en/modules.php
@@ -12,6 +12,7 @@
 
 /* Back end modules */
 $GLOBALS['TL_LANG']['MOD']['modal_gallery'] = array('Modal Gallery', 'Add slides to the modal gallery');
+$GLOBALS['TL_LANG']['MOD']['slide_categories'] = array('Slide Categories', 'Create and manage the categories for the modal gallery slides');
 
 /* Front end modules */
 $GLOBALS['TL_LANG']['FMD']['modal_gallery_module'] 	= array('Modal Gallery', 'The front end module that displays the gallery.');
