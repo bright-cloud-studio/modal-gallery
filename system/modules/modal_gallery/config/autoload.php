289559878ff0659b37c50674348eb51c2b2f2d76
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-13 08:57:06 -0500

    Update autoload.php

--- system/modules/modal_gallery/config/autoload.php
+++ system/modules/modal_gallery/config/autoload.php
@@ -13,15 +13,16 @@
 /* Register the classes */
 ClassLoader::addClasses(array
 (
-	'Bcs\Module\ModalGalleryModule' 				=> 'system/modules/modal_gallery/library/Bcs/Module/ModalGalleryModule.php',
-	'Bcs\Model\ModalGallery' 						=> 'system/modules/modal_gallery/library/Bcs/Model/ModalGallery.php',
-	'Bcs\Model\ModalGallerySlide' 						=> 'system/modules/modal_gallery/library/Bcs/Model/ModalGallerySlide.php',
-	'Bcs\Backend\HotspotHelper' 					=> 'system/modules/modal_gallery/library/Bcs/Backend/HotspotHelper.php'
+	'Bcs\Module\ModalGalleryModule' 	=> 'system/modules/modal_gallery/library/Bcs/Module/ModalGalleryModule.php',
+	'Bcs\Model\ModalGallery' 		=> 'system/modules/modal_gallery/library/Bcs/Model/ModalGallery.php',
+	'Bcs\Model\ModalGallerySlide' 		=> 'system/modules/modal_gallery/library/Bcs/Model/ModalGallerySlide.php',
+	'Bcs\Backend\HotspotHelper' 		=> 'system/modules/modal_gallery/library/Bcs/Backend/HotspotHelper.php'
 ));
 
 /* Register the templates */
 TemplateLoader::addFiles(array
 (
-	'modal_gallery_module' 		=> 'system/modules/modal_gallery/templates/modules',
+	'modal_gallery_module' 			=> 'system/modules/modal_gallery/templates/modules',
 	'item_slide' 				=> 'system/modules/modal_gallery/templates/items',
+	'picture_default_modal_thumb' 		=> 'system/modules/modal_gallery/templates/items',
 ));
