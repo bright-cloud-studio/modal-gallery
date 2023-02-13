250d126b2042a1d3e897b952819ce95e2c8f212b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-13 10:29:08 -0500

    Create tl_slide_category.php

---
+++ system/modules/modal_gallery/languages/en/tl_slide_category.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Modal Gallery
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/modal-gallery
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_slide_category']['category_legend'] 	= 'Slide Category Details';
+$GLOBALS['TL_LANG']['tl_slide_category']['publish_legend'] 	  = 'Publish';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_slide_category']['alias'] 			= array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_slide_category']['name'] 				= array('Slide Category Name', 'This categories label');
+$GLOBALS['TL_LANG']['tl_slide_category']['published'] 	= array('Published', 'Show this record on the front end.');
+ 
+ /* Buttons */
+$GLOBALS['TL_LANG']['tl_slide_category']['new']   			= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_slide_category']['show']  		 	= array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_slide_category']['edit']  		 	= array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_slide_category']['copy']  		 	= array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_slide_category']['delete'] 			= array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_slide_category']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
