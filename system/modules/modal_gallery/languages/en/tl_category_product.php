f9d86c123f0d7166ef7649cb14399331620cab50
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-15 09:35:34 -0500

    Create tl_category_product.php

---
+++ system/modules/modal_gallery/languages/en/tl_category_product.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Modal Gallery
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/modal-gallery
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_category_product']['category_legend'] 	= 'Product Category Details';
+$GLOBALS['TL_LANG']['tl_category_product']['publish_legend'] 	  = 'Publish';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_category_product']['alias'] 			= array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_category_product']['name'] 				= array('Slide Category Name', 'Display name for this product category');
+$GLOBALS['TL_LANG']['tl_category_product']['published'] 	= array('Published', 'Show this product category on the front end.');
+ 
+ /* Buttons */
+$GLOBALS['TL_LANG']['tl_category_product']['new']   			= array('New Product Category', 'Add a new product category');
+$GLOBALS['TL_LANG']['tl_category_product']['show']  		 	= array('Product Category details', 'Show the details of product category ID %s');
+$GLOBALS['TL_LANG']['tl_category_product']['edit']  		 	= array('Edit Product Category', 'Edit product category ID %s');
+$GLOBALS['TL_LANG']['tl_category_product']['copy']  		 	= array('Copy Product Category', 'Copy product category ID %s');
+$GLOBALS['TL_LANG']['tl_category_product']['delete'] 			= array('Delete Product Category', 'Delete product category ID %s');
+$GLOBALS['TL_LANG']['tl_category_product']['toggle'] 			= array('Toggle Product Category', 'Toggle product category ID %s');
