123608f66b994afa9904d47a070f37f703eea03e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-15 11:03:55 -0500

    Update tl_module.php

--- system/modules/modal_gallery/languages/en/tl_module.php
+++ system/modules/modal_gallery/languages/en/tl_module.php
@@ -1,9 +1,10 @@
 
 <?php
  
+/* Legends */
+$GLOBALS['TL_LANG']['tl_module']['select_gallery_legend'] 	= "Select a Gallery";
+$GLOBALS['TL_LANG']['tl_module']['empty_message_legend'] 	= "Empty Message";
 
 /* Fields */
 $GLOBALS['TL_LANG']['tl_module']['selectedGallery'] 		= array('Galleries Available', 'Select which available gallery this front end module will display.');
-
-/* Legends */
-$GLOBALS['TL_LANG']['tl_module']['select_gallery_legend'] 	= "Select a Gallery";
+$GLOBALS['TL_LANG']['tl_module']['empty_message'] 		= array('Empty Message', 'The message that displays when no products meet the filter\'s criteria');
