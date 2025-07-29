5660c00a305a2462a54b7fb0bf2944dc8d34b961
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-29 12:51:40 -0400

    Create ModalGallery.php

---
+++ src/Resources/contao/library/Bcs/Model/ModalGallery.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Modal Gallery
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/modal-gallery
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+ 
+namespace Bcs\Model;
+
+class ModalGallery extends \Model
+{
+	
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_modal_gallery';
+
+	
+}
