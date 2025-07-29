68fcb61993001578fae3a86ed6aa352db693f78e
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-29 12:38:24 -0400

    Create tl_modal_gallery_slide.php

---
+++ src/Resources/contao/languages/en/tl_modal_gallery_slide.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Modal Gallery
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/modal-gallery
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['slide_legend']        	= 'Slide Details';
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['hotspot_legend']      	= 'Hotspot Details';
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['category_legend']      = 'Category Details';
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['publish_legend']      	= 'Publish';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['alias'] 			            = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['product_name'] 			     = array('Product Name', 'The name of the product');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['product_inventory'] 	 	= array('Product Inventory', 'The inventory total');
+
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['slide_name'] 	       		= array('Slide Name', 'Give this slide a name');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['slide_image'] 	      		= array('Slide Image', 'Select the image we will use for this particular slide');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['slide_image_url'] 	    = array(' ', ' ');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['slide_image_meta'] 	   = array('Slide Image Meta', 'Set the meta details');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['hotspot_icon'] 	      	= array('Hotspot Icon', 'Insert code for the hotspot links\'s icon');
+
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['hotspot_links']     	 	= array('Hotspot Links', 'These will display as hotspot icons on top of the slide image, when clicked the text will display in a modal');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['hotspot_x']         	 	= array('Hotspot X', 'X coordinate for the hotspot link on the image');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['hotspot_y'] 	        	 = array('Hotspot Y', 'Y coordinate for the hotspot link on the image');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['hotspot_title']      		= array('Hotspot Title', 'The text that will be the title for the modal box');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['hotspot_text']      	 	= array('Hotspot Text', 'The text that will show in the modal box');
+
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['category_room'] 	       		  = array('Room Category', 'Select the room categories this slide belongs to');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['category_product'] 	       		  = array('Product Category', 'Select the product categories this slide belongs to');
+
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['published'] 		         = array('Published', 'Save this product\'s record.');
+ 
+ /* Buttons */
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['new']   		 			         = array('New Slide', 'Add a new slide');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['show']  		 		         	= array('Slide Details', 'Show the details of slide ID %s');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['edit']  		          			= array('Edit Slide', 'Edit slide ID %s');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['copy']  		 	         		= array('Copy Slide', 'Copy slide ID %s');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['cut']  		 	         		= array('Move Slide', 'Move slide ID %s');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['delete'] 		         			= array('Delete Slide', 'Delete slide ID %s');
+$GLOBALS['TL_LANG']['tl_modal_gallery_slide']['toggle'] 			         		= array('Toggle Slide Published', 'Toggle slide ID %s');
