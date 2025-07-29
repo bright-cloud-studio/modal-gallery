60c8f78a8ba62baabba585d07ee11703a3fc4405
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-29 11:52:26 -0400

    Create tl_module.php

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Modal Gallery
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/modal-gallery
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Add a palette to tl_module */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['modal_gallery_module'] 		= '{title_legend},name,headline,type;{select_gallery_legend},selectedGallery;{empty_message_legend},empty_message;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['selectedGallery'] = array
+(
+	'label' 			=> &$GLOBALS['TL_LANG']['tl_module']['selectedGallery'],
+	'inputType' 			=> 'select',
+	'options_callback'		=> array('Bcs\Backend\ModalGalleryBackend', 'getGalleries'),
+	'eval' 				=> array('tl_class'=>'clr w50'),
+	'sql' 				=> "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_module']['fields']['empty_message'] = array
+(
+	'label' 			=> &$GLOBALS['TL_LANG']['tl_module']['empty_message'],
+	'inputType' 			=> 'text',
+	'eval' 				=> array('tl_class'=>'clr w50'),
+	'sql' 				=> "varchar(255) NOT NULL default ''"
+);
