498575b2c93994efb0104f6d887ba6406a1aaa49
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-29 11:33:54 -0400

    Create BcsModalGalleryBundle.php

---
+++ src/BcsModalGalleryBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao OSI
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/modal-gallery
+ */
+
+namespace Bcs\ModalGalleryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsModalGalleryBundle extends Bundle
+{
+}
