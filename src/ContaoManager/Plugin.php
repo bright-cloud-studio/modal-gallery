e913850b4c915343deb6c53351caa02881849e7a
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-29 11:34:59 -0400

    Create Plugin.php

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bcs\ModalGalleryBundle\ContaoManager;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create('Bcs\ModalGalleryBundle\BcsModalGalleryBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+        ];
+    }
+}
