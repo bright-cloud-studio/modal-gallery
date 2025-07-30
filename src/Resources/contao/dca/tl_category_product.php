22589a2c56b0f1865484f20d371610ffe6b3b103
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-30 09:14:31 -0400

    Update tl_category_product.php

--- src/Resources/contao/dca/tl_category_product.php
+++ src/Resources/contao/dca/tl_category_product.php
@@ -1,14 +1,6 @@
 <?php
 
-/**
- * Bright Cloud Studio's Modal Gallery
- *
- * Copyright (C) 2023 Bright Cloud Studio
- *
- * @package    bright-cloud-studio/modal-gallery
- * @link       https://www.brightcloudstudio.com/
- * @license    http://opensource.org/licenses/lgpl-3.0.html
-**/
+use Contao\DC_Table;
 
  
 /* Table tl_slide_category */
@@ -18,7 +10,7 @@ $GLOBALS['TL_DCA']['tl_category_product'] = array
     // Config
     'config' => array
     (
-        'dataContainer'               => 'Table',
+        'dataContainer'               => DC_Table::class,
         'enableVersioning'            => true,
         'sql' => array
         (
