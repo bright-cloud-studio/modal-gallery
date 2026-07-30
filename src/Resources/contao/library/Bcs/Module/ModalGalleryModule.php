bacee389c9a23f52135ad53406a8240d73bf2034
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-07-30 11:36:59 -0400

    Update ModalGalleryModule.php

--- src/Resources/contao/library/Bcs/Module/ModalGalleryModule.php
+++ src/Resources/contao/library/Bcs/Module/ModalGalleryModule.php
@@ -12,6 +12,7 @@
 
 namespace Bcs\Module;
 
+use Bcs\Model\ModalGallery;
 use Bcs\Model\ModalGallerySlide;
 
 use Contao\Database;
@@ -27,6 +28,7 @@ class ModalGalleryModule extends \Contao\Module
      * @var string
      */
     protected $strTemplate = 'modal_gallery_module';
+    protected $objGallery;
 
     /**
      * Initialize the object
@@ -61,6 +63,14 @@ class ModalGalleryModule extends \Contao\Module
             return $objTemplate->parse();
         }
 
+        // Pick the module template from the gallery's chosen style
+        $this->objGallery = ModalGallery::findByPk($this->selectedGallery);
+    
+        if (!$this->customTpl && $this->objGallery !== null && $this->objGallery->template_style != 'vertical')
+        {
+            $this->strTemplate = 'modal_gallery_module_' . $this->objGallery->template_style;
+        }
+    
         return parent::generate();
     }
 
@@ -112,9 +122,7 @@ class ModalGalleryModule extends \Contao\Module
 
         $entry_id = 1;
         
-        $modal_gallery = Database::getInstance()
-                           ->prepare("SELECT * FROM tl_modal_gallery WHERE id=?")
-                           ->execute($this->selectedGallery);
+        //$modal_gallery = Database::getInstance()->prepare("SELECT * FROM tl_modal_gallery WHERE id=?")->execute($this->selectedGallery);
         
         // Loop through slides
         foreach ($objSlides as $slide)
@@ -154,11 +162,11 @@ class ModalGalleryModule extends \Contao\Module
             $arrSlide['categories_room']    = unserialize($slide->category_room);
             $arrSlide['categories_product'] = unserialize($slide->category_product);
 
-            if ($modal_gallery->numRows)
+            if ($this->objGallery->numRows)
             {
-                $arrSlide['size_thumb']   = unserialize($modal_gallery->slide_thumb_image_size);
-                $arrSlide['size_slide']   = unserialize($modal_gallery->slide_image_size);
-                $arrSlide['hotspot_icon'] = $modal_gallery->hotspot_icon;
+                $arrSlide['size_thumb']   = unserialize($this->objGallery->slide_thumb_image_size);
+                $arrSlide['size_slide']   = unserialize($this->objGallery->slide_image_size);
+                $arrSlide['hotspot_icon'] = $this->objGallery->hotspot_icon;
             }
 
             // Generate thumb template
@@ -225,12 +233,12 @@ class ModalGalleryModule extends \Contao\Module
         $this->Template->slides            = $arrSlides;
 
         $this->Template->slides_serialized = $jsonResult = json_encode($slides_comparison);
-        $this->Template->accordion_active_percentage = $modal_gallery->accordion_active_percentage;
+        $this->Template->accordion_active_percentage = $this->objGallery->accordion_active_percentage;
         
         $this->Template->categories_room   = $arrCategoriesRoom;
         $this->Template->categories_product= $arrCategoriesProduct;
         
-        $this->Template->gallery_style = $modal_gallery->template_style;
+        $this->Template->gallery_style = $this->strTemplate;
         
     }
 }
