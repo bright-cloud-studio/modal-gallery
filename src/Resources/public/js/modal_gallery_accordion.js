af8a637682ffd6e22934fb0b218bbb0385942e12
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-08-04 10:05:28 -0400

    Update modal_gallery_accordion.js

--- src/Resources/public/js/modal_gallery_accordion.js
+++ src/Resources/public/js/modal_gallery_accordion.js
@@ -1,5 +1,3 @@
-const activeEl = document.querySelector('.accordion_active_percentage');
-  if (!activeEl) return;
 const percentage = parseFloat(document.querySelector('.accordion_active_percentage').textContent);
 
 document.documentElement.style.setProperty('--main-percent', percentage);
