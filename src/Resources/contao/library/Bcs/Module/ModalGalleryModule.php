d1ce8e0ea22adcc111e943f88976bd2b9c089ba5
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-08-06 10:14:20 -0400

    Update ModalGalleryModule.php

--- src/Resources/contao/library/Bcs/Module/ModalGalleryModule.php
+++ src/Resources/contao/library/Bcs/Module/ModalGalleryModule.php
@@ -257,6 +257,10 @@ class ModalGalleryModule extends \Contao\Module
         $this->Template->canvas_width  = (int) $this->modal_gallery->comparison_canvas_width  ?: 800;
         $this->Template->canvas_height = (int) $this->modal_gallery->comparison_canvas_height ?: 500;
 
+        // Starting share of the first slide. Zero means "no preference", which
+        // the script reads as an even split across every slide.
+        $this->Template->first_percentage = (int) $this->modal_gallery->comparison_first_percentage;
+
 
         $this->Template->categories_room   = $arrCategoriesRoom;
         $this->Template->categories_product= $arrCategoriesProduct;
